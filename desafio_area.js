82ca288611da040251f85ae8bebace7cfdd2c52d
arthur-gui-22 <142233435+arthur-gui-22@users.noreply.github.com>
2024-01-04 20:02:53 -0300

    Add files via upload

---
+++ desafio_area.js
@@ -0,0 +1,10 @@
+// PI * raio * raio
+
+const PI = 3.14;
+let raio = 20;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da área é " + areaCirc + "m2.");
+
+areaCir = Math.PI * raio * raio;
+console.log("O valor da área é " + areaCirc + "m2.");
