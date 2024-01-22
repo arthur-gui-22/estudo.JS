568cc76b1d71451ba893d50fc0d3fbcfeeb3f271
arthur-gui-22 <142233435+arthur-gui-22@users.noreply.github.com>
2024-01-22 20:56:37 -0300

    Add files via upload

---
+++ desafio_atribuicao.js
@@ -0,0 +1,9 @@
+// Criar uma piquena lista de aprovados.
+
+let texto ='Lista de Aprovados';
+texto += '\n\n===================';
+texto += '\n1.Ana Silva';
+texto += '\n2.Pedro Albuquerque';
+texto += '\n3.Guilherme Pereira';
+texto += '\n4.Rebeca França';
+console.log(texto);
