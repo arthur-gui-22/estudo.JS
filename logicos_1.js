0c57ce0f8330d517e0b9156ea3ab3e2b09356074
arthur-gui-22 <142233435+arthur-gui-22@users.noreply.github.com>
2024-02-13 20:19:44 -0300

    Add files via upload

---
+++ logicos_1.js
@@ -0,0 +1,13 @@
+let temDinheiro = true;
+let estaEnsolarado = true;
+let carroEstaNaGaragem = false;
+
+let resultadoE = '#1 (AND)- Vai pro shopping?';
+resultadoE += temDinheiro && estaEnsolarado;
+
+console.log(resultadoE);
+
+let resultadoOU = '#2 (OR)- Vai pro shopping?';
+resultadoOU += estaEnsolarado || carroEstaNaGaragem;
+
+console.log(resultadoOU);
