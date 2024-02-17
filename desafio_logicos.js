8432c96cb4edac69320665d0fc71ea49522694f5
arthur-gui-22 <142233435+arthur-gui-22@users.noreply.github.com>
2024-02-17 00:26:11 -0300

    Add files via upload

---
+++ desafio_logicos.js
@@ -0,0 +1,15 @@
+// Duas expressões que resultar em TRUE (Aritiméticos/Relacionais/lógicos)
+
+let verdadeira1 = 5 + 2 >= 7 && 8 > 2 ;
+let verdadeira2 = !(5 * 2 == 10 > 7) ;
+
+console.log( verdadeira1);
+console.log(verdadeira2);
+
+// Duas expressões que resultar em FALSE (Aritiméticos/Relacionais/lógicos)
+
+let falso1 = 7 - 2 != 5 || 4 < 2;
+let falso2 = !(7 !== 7 || '7' !== 7);
+
+console.log(falso1);
+console.log(falso2);
