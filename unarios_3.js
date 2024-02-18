c0ff05037280409ef32721fec4a97ae0f626a230
arthur-gui-22 <142233435+arthur-gui-22@users.noreply.github.com>
2024-02-17 22:20:01 -0300

    Add files via upload

---
+++ unarios_3.js
@@ -0,0 +1,16 @@
+// dupla negação com valor bullion.
+
+console.log(!!true);
+console.log(!!false);
+
+// dupla negação com valor literal. 
+
+console.log(!!'texto');
+console.log(!!"");
+
+// dupla negação com valor numérico.
+
+console.log(!!1);
+console.log(!!0);
+console.log(!!-1);
+console.log(!!-0.00001);
