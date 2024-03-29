bd3af046549f738d7036d73bdd3c2a697e5b126c
arthur-gui-22 <142233435+arthur-gui-22@users.noreply.github.com>
2024-03-28 22:35:51 -0300

    Add files via upload

---
+++ while_4.js
@@ -0,0 +1,10 @@
+// declaração; expreção; incremento -> Quantidade determinada!
+
+let controle = 1;
+
+while(controle <= 10){
+    console.log(controle);
+    controle++;
+}
+
+console.log('Fim!!!');
