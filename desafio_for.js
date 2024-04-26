45f7827768a7f2af8c72696497cc7e79daf78741
arthur-gui-22 <142233435+arthur-gui-22@users.noreply.github.com>
2024-04-26 00:54:16 -0300

    Add files via upload

---
+++ desafio_for.js
@@ -0,0 +1,19 @@
+//#
+//##
+//###
+//####
+//#####
+//######
+
+// Com valor numérico 
+let linha = '';
+
+for(let i = 1; i<= 6; i++){
+    linha += '#'
+    console.log(linha);
+}
+
+// Sem valor numérico 
+for(let s = '#'; s != '#######'; s+= '#'){
+    console.log(s);
+}
